a9668796391d422f016b9cd026de212965d301c6
Artsem Paulenka <itemka2503@gmail.com>
2024-11-03 16:51:16 +0100

    Add react-router-dom

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,6 +2,8 @@ import { useAppDispatch, useAppSelector } from '../../hooks/storeHooks.ts';
 import './styles.css';
 import { dashboardValueSelector } from '../../redux/dashboard/selectors.ts';
 import { increment } from '../../redux/dashboard/dashboardSlice.ts';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 function App() {
   const dispatch = useAppDispatch();
@@ -12,11 +14,31 @@ function App() {
   };
 
   return (
-    <div>
-      <h1>Virtual machines dashboard</h1>
-      <p>{value}</p>
-      <button onClick={handleClick}>Increment value</button>
-    </div>
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <div>
+              Home
+              <br />
+              <Link to="dashboard">Dashboard</Link>
+            </div>
+          }
+        />
+        <Route
+          path="/dashboard"
+          element={
+            <>
+              <h1>Virtual machines dashboard</h1>
+              <p>{value}</p>
+              <button onClick={handleClick}>Increment value</button>
+            </>
+          }
+        />
+        <Route path="*" element={<>Not found</>} />
+      </Routes>
+    </Router>
   );
 }
 
